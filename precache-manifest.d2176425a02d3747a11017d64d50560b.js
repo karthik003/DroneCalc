7bb4d7bda1e7ba454a1cc7b3088e789cb9dcd6e3
Karthik V.J <vjk2018@gmail.com>
2020-06-20 21:09:30 +0530

    Updates

---
+++ precache-manifest.d2176425a02d3747a11017d64d50560b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa8f3f89a8b37683050ae77d45bbe48",
+    "url": "/DroneCalc/index.html"
+  },
+  {
+    "revision": "e9a30387d030a9c517bd",
+    "url": "/DroneCalc/static/css/2.1488431a.chunk.css"
+  },
+  {
+    "revision": "0fcdf42c2af5bec54624",
+    "url": "/DroneCalc/static/css/main.dbcfb6c1.chunk.css"
+  },
+  {
+    "revision": "e9a30387d030a9c517bd",
+    "url": "/DroneCalc/static/js/2.df9c3109.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/DroneCalc/static/js/2.df9c3109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fcdf42c2af5bec54624",
+    "url": "/DroneCalc/static/js/main.9a731e78.chunk.js"
+  },
+  {
+    "revision": "f7f1cecc739f108a3261",
+    "url": "/DroneCalc/static/js/runtime-main.23c9c69e.js"
+  },
+  {
+    "revision": "c641d3e90c64fcb82f8f5e2ac5317e98",
+    "url": "/DroneCalc/static/media/Orbitron-VariableFont_wght.c641d3e9.ttf"
+  },
+  {
+    "revision": "c9876fee428dd31e92f61853896d5545",
+    "url": "/DroneCalc/static/media/drone1.c9876fee.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DroneCalc/static/media/logo.5d5d9eef.svg"
+  }
+]);
